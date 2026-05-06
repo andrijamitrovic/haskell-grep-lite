a0daebb57facd59d4efad5e37c7d2f7112e89d32
Andrija Mitrovic <andrijamitrovic11@gmail.com>
2026-05-06 11:54:46 +0200

    added parsers for Regex types

--- app/Regex/Parser.hs
+++ app/Regex/Parser.hs
@@ -2,6 +2,7 @@ module Regex.Parser where
 
 import Control.Applicative
 import Regex.Core
+import Data.Char (isAlphaNum)
 
 newtype Parser a = Parser 
     { runParser :: String -> Maybe (a, String)
@@ -48,6 +49,16 @@ instance Alternative Parser where
                 success -> 
                     success
 
+instance Monad Parser where
+  parser >>= f =
+    Parser $ \input ->
+      case runParser parser input of
+        Nothing ->
+          Nothing
+        Just (value, rest) ->
+          runParser (f value) rest
+
+
 item :: Parser Char 
 item = Parser $ \input ->
     case input of 
@@ -68,3 +79,44 @@ satisfy predicate = Parser $ \input ->
 char :: Char -> Parser Char
 char c = 
     satisfy (== c)
+
+symbol :: Parser Reg
+symbol = Sym <$> satisfy isLiteral
+
+isLiteral :: Char -> Bool
+isLiteral c = 
+    isAlphaNum c
+
+parseOnly :: Parser a -> String -> Maybe a
+parseOnly parser input = 
+    case runParser parser input of 
+        Just (value, "") -> 
+            Just value
+        
+        _ -> 
+            Nothing
+
+concatRegex :: Parser Reg
+concatRegex =
+  foldSeq <$> some repeatRegex
+
+foldSeq :: [Reg] -> Reg
+foldSeq [] = 
+    Eps
+foldSeq [regex] = 
+    regex
+foldSeq (regex : regexes) = 
+    Seq regex (foldSeq regexes)
+
+optionalChar :: Char -> Parser Bool
+optionalChar c =
+  (True <$ char c) <|> pure False
+
+repeatRegex :: Parser Reg
+repeatRegex = do
+  regex <- symbol
+  hasStar <- optionalChar '*'
+  pure $
+    if hasStar
+      then Rep regex
+      else regex
